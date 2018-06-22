9983bf9c0c42d56db403f554382ae056b3d31993
DimNk <nk197777777777@mail.ru>
2018-06-22 19:40:37 +0300

    3rd commit

--- GitTest/GitTest/Program.cs
+++ GitTest/GitTest/Program.cs
@@ -8,8 +8,15 @@ namespace GitTest
 {
     class Program
     {
+        //int i;
+        //public int I1 { get => i; set => i = value; }
+        int I{get;set;}
         static void Main(string[] args)
         {
+            Program program = new Program();
+            Console.WriteLine(program.GetType());
+
+            Console.ReadKey();
         }
     }
 }
